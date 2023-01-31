94af222c3d6f125ab734065881faaebddb83678b
Olegani4 <105884259+Olegani4@users.noreply.github.com>
2023-01-31 23:22:25 +0200

    Update ListProcessingWithStreams.java

--- src/lv/tsi/lamda/ListProcessingWithStreams.java
+++ src/lv/tsi/lamda/ListProcessingWithStreams.java
@@ -8,7 +8,9 @@ public class ListProcessingWithStreams {
     public static void main(String[] args) {
         ArrayList<String> elemList = new ArrayList<>(Arrays.asList("a1", "c3", "a2", "a3", "b3",
                 "b2", "c1", "c2", "b1"));
-        System.out.println("The first element: " + elemList.get(0));
+
+
+        System.out.println("The first element: " + elemList.stream().findFirst().get());
 
 
         elemList.replaceAll(String::toUpperCase);
